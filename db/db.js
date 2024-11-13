607fb487613c8abb2ec14a22b800556a99d51ddd
GaneshDandekar26 <ganeshdandekar2004@gmail.com>
2024-11-13 09:36:22 +0530

    COMMIT

--- db/db.js
+++ db/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql');
 const mySqlConnection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "immOmi14",
+  password: "262004",
   database: "Hotel_Management"
 });
 
